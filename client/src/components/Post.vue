992525059b1417abe6814283ac073a3d56abd2d2
psihoshlem <yungync@gmail.com>
2023-09-05 10:22:17 +0300

    posts style

--- client/src/components/Post.vue
+++ client/src/components/Post.vue
@@ -2,17 +2,26 @@
     <div class="post">
       <h4>{{ post.title }}</h4>
       <p>{{ post.text }}</p>
-      <p>Дата: {{ post.date }}</p>
+      <p class="date">{{ post.date }}</p>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      post: Object
-    }
-  };
-  </script>
-  
-  <style scoped>
-  </style>
+</template>
+
+<script>
+export default {
+  props: {
+    post: Object
+  }
+};
+</script>
+
+<style scoped>
+.post{
+  background-color: #fff;
+  margin: 20px;
+  padding: 20px;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
+}
+.date{
+  color: rgb(161, 161, 161);
+}
+</style>
